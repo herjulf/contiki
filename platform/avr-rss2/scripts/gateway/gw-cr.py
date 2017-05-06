5b3f0900d2e662f6cd70a19a54408b5fde579c85
Christian Rohner <ch_rohner@yahoo.se>
2017-05-06 14:08:09 +0200

    new gateway: open and write command to serial port, no reading yet

--- platform/avr-rss2/scripts/gateway/gw-cr.py
+++ platform/avr-rss2/scripts/gateway/gw-cr.py
@@ -85,7 +85,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
       recordID = int(self.path.split('/')[-1])
       serialWrite(LocalData.nodes[recordID]['serialPath'], LocalData.nodes[recordID]['serialBaudrate'], data)
       LocalData.records[recordID] = data
-      print "record %s is added successfully" % recordID
+      print "sent %s to node %s" % (str(data), recordID)
       self.send_response(200)
       self.end_headers()
 
