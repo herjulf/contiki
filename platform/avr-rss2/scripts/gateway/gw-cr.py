53e2188b54c1bdc88d17fe0173a7ec019c752a99
Christian Rohner <ch_rohner@yahoo.se>
2017-05-06 12:42:21 +0200

    new gateway: read config file. still need to handle serial communication

--- platform/avr-rss2/scripts/gateway/gw-cr.py
+++ platform/avr-rss2/scripts/gateway/gw-cr.py
@@ -4,33 +4,81 @@ import threading
 import argparse
 import re
 import cgi
+import urlparse
+import json
 
 import subprocess
 
-serialNumber = subprocess.check_output(['cat', '/sys/bus/usb/devices/1-1.5/serial'])
-ps = subprocess.Popen(('ls', '/sys/bus/usb/devices/1-1.5:1.0/'), stdout=subprocess.PIPE)
-path = subprocess.check_output(('grep', 'ttyUSB'), stdin=ps.stdout)
-print(serialNumber, path)
 
+# example configuration
+# {
+#      "httpPort" : 12100,
+#      "moteNumber" : 10,
+#      "moteSerialID" : "49617",
+#      "serialBaudrate" : 38400,
+#      "moteAddress" : "0.0",
+#      "moteType" : "avr-rss2",
+#      "logLevel" : 4,
+#      "logFile" : "log.log"
+#  }
+
+
+
+def initializeNodes():
+  # go through nodes data structure and map usb port to the devices
+  # return HTTP response code
+
+  for i in {2, 3, 4, 5}:
+    # loop through USB interfaces
+#    serial = subprocess.check_output(['cat', '/sys/bus/usb/devices/1-1.5/serial'])
+#    ps = subprocess.Popen(('ls', '/sys/bus/usb/devices/1-1.5:1.0/'), stdout=subprocess.PIPE)
+#    path = subprocess.check_output(('grep', 'ttyUSB'), stdin=ps.stdout)
+    serial = "49617"
+    path = "/dev/ttyUSB" + str(i-2)
+    print(serial, path)
+
+    # find node with serial
+    for n in LocalData.nodes:
+      if (n['moteSerialID'] == serial):
+        n['serialPort'] = path;
+        print("found")
+
+  return 200
 
  
 class LocalData(object):
   records = {}
+  nodes = {}
+  logs = {}
  
+
 class HTTPRequestHandler(BaseHTTPRequestHandler):
   def do_POST(self):
-    if None != re.search('/api/v1/addrecord/*', self.path):
+    if None != re.search('/api/nodes/config', self.path):
+      # node configuration {nodeId, baudrate, [position, ...]}, in particular mapping nodeId and baudrate
       ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-      if ctype == 'application/json':
-        length = int(self.headers.getheader('content-length'))
-        data = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
-        recordID = self.path.split('/')[-1]
-        LocalData.records[recordID] = data
-        print "record %s is added successfully" % recordID
-      else:
-        data = {}
+      length = int(self.headers.getheader('content-length'))
+      data = json.loads(self.rfile.read(length))
+      # TODO: sanity check ...
+      LocalData.nodes = data['motes']
+      print "configuration received"
+      # map nodeId to usb port
+      ret = initializeNodes()
+      print LocalData.nodes
+      self.send_response(ret)
+      self.end_headers()
+
+    elif None != re.search('/api/node/*', self.path):
+      # command to be sent to node n
+      ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+      length = int(self.headers.getheader('content-length'))
+      data = urlparse.parse_qs(self.rfile.read(length), keep_blank_values=1)
+      recordID = self.path.split('/')[-1]
+      LocalData.records[recordID] = data
+      print "record %s is added successfully" % recordID
       self.send_response(200)
       self.end_headers()
+
     else:
       self.send_response(403)
       self.send_header('Content-Type', 'application/json')
@@ -38,17 +86,24 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
     return
  
   def do_GET(self):
-    if None != re.search('/api/v1/getrecord/*', self.path):
-      recordID = self.path.split('/')[-1]
-      if LocalData.records.has_key(recordID):
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(LocalData.records[recordID])
-      else:
-        self.send_response(400, 'Bad Request: record does not exist')
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
+    if None != re.search('/api/nodes/count', self.path):
+      self.send_response(200)
+#      self.send_header('Content-Type', 'application/json')
+      self.end_headers()
+      self.wfile.write(len(LocalData.records))
+
+    elif None != re.search('/api/nodes', self.path):
+      self.send_response(200)
+#      self.send_header('Content-Type', 'application/json')
+      self.end_headers()
+      self.wfile.write(LocalData.records)
+
+    elif None != re.search('/api/logs', self.path):
+      self.send_response(200)
+#      self.send_header('Content-Type', 'application/json')
+      self.end_headers()
+      self.wfile.write(LocalData.logs)
+
     else:
       self.send_response(403)
       self.send_header('Content-Type', 'application/json')
